2169609ed9616b6c18836ff98f2514166114aba9
wuxianbin <wuguanzhen1985@sina.cn>
2019-04-04 16:56:55 +0800

    123

--- sell-web/sell-web-api/src/main/java/com/jellyfish/sell/api/SellApiApp.java
+++ sell-web/sell-web-api/src/main/java/com/jellyfish/sell/api/SellApiApp.java
@@ -4,9 +4,14 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.ImportResource;
 
+/**
+ * Demo class
+ *
+ * @author keriezhang
+ * @date 2016/10/31
+ */
 @SpringBootApplication
 @ComponentScan(basePackages={"com.jellyfish.sell.*"})
 @MapperScan({"com.jellyfish.sell.*.mapper"})
